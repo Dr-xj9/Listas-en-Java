37584965b4c298f75ae76d1de510b4bc46eff9d3
Daniel <romeroibarradaniel@gmail.com>
2024-09-04 22:32:13 -0600

    Terminado

--- ListaNodos.java
+++ ListaNodos.java
@@ -1,9 +1,9 @@
 /**
- *
  * @author Daniel
- */
+**/
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
@@ -13,43 +13,41 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 class Lista {
+    private ArrayList<Productos> list;
     
-	private ArrayList<Productos> list = new ArrayList<>();
-    private int index,posicion;
-	
-    public Lista(ArrayList<Productos> list){
-        this.list=list;
+    public Lista(ArrayList<Productos> list) {
+        this.list = list;
     }
-    public ArrayList<Productos> Actualizar(){
-		return this.list;
-	}
-	public void Agregar(Productos p){
-		this.list.add(p);
-	}
-	public Productos Buscar(String codigo){
-		Productos ob=new Productos();
-		this.index=0;
-		for(Productos x: this.list){
-			if(codigo.equals(x.getCodigo()))
-			{
-				ob.setCodigo(x.getCodigo());
-				ob.setDescripcion(x.getDescripcion());
-				ob.setPrecio(x.getPrecio());
-				ob.setCantidad(x.getCantidad());
-				this.posicion=this.index;
-			}
-			this.index++;
-		}
-		return ob;
-	}
-	public int getPosicion(){
-		return this.posicion;
-	}
-	public void Remover(int index){
-		this.list.remove(index);
-	}
+    
+    public ArrayList<Productos> Actualizar() {
+        return this.list;
+    }
+    
+    public void Agregar(Productos p) {
+        this.list.add(p);
+    }
+    
+    public Productos Buscar(String codigo) {
+        for (Productos x : this.list) {
+            if (codigo.equals(x.getCodigo())) {
+                return x;
+            }
+        }    
+        return null;
+    }
+    
+    public int Remover(String codigo) {
+        Iterator<Productos> iterator = this.list.iterator();
+        while (iterator.hasNext()) {
+            Productos x = iterator.next();
+            if (codigo.equals(x.getCodigo())) {
+                iterator.remove();
+                return 0; // Éxito
+            }
+        }
+        return 1; // No se encontró el producto
+    }
 }
-
 
 class Productos {
     private String codigo;
@@ -77,7 +75,6 @@ class Productos {
 	public void setCantidad(Double cantidad) {
 		this.cantidad = cantidad;
 	}
-	
 	public Double getPrecio() {
 		return precio;
 	}
@@ -87,17 +84,12 @@ class Productos {
 }
 
 public class ListaNodos extends JFrame implements ActionListener {
-
-    /**
-     * @param args the command line arguments
-     */
 	private JLabel etiqueta;
 	private JButton botonSalvar;
 	private JButton botonBuscar;
 	private JButton botonRemover;
 	private JButton botonNuevo;
 	private JButton botonEditar;
-	
 	private JTextField codigoBuscar;
 	private JTextField codigo;
 	private JTextField descripcion;
@@ -105,12 +97,18 @@ public class ListaNodos extends JFrame implements ActionListener {
 	private JTextField cantidad;
 	private JTextField total;
 	
+	private ArrayList<Productos> listaP = new ArrayList<Productos>(); 
+	private Productos producto = new Productos();
+	private Lista lista = new Lista(listaP);
+	
 	public ListaNodos(){
 		this.setBounds(100, 100, 500, 500);
 		this.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		etiqueta = new JLabel();
-		etiqueta.setBounds(125, 10, 300, 30);
+						//  X,  Y ,Width,Height
+		etiqueta.setBounds(200, 10, 300, 30);
 		etiqueta.setName("etiquetaTitulo");
 		etiqueta.setText("Lista de Productos");
 		etiqueta.setVisible(true);
@@ -124,14 +122,14 @@ public class ListaNodos extends JFrame implements ActionListener {
 		this.add(etiqueta);
 		
 		codigoBuscar = new JTextField();
-		codigoBuscar.setBounds(110, 50, 180, 30);
+		codigoBuscar.setBounds(110, 50, 250, 30);
 		codigoBuscar.setName("codigoBuscar");
 		codigoBuscar.setText("");
 		codigoBuscar.setVisible(true);
 		this.add(codigoBuscar);
 		
 		botonBuscar = new JButton();
-		botonBuscar.setBounds(300, 50, 90,30);
+		botonBuscar.setBounds(370, 50, 90,30);
 		botonBuscar.setName("botonBuscar");
 		botonBuscar.setText("Buscar");
 		botonBuscar.setVisible(true);
@@ -160,7 +158,7 @@ public class ListaNodos extends JFrame implements ActionListener {
 		this.add(etiqueta);
 		
 		descripcion = new JTextField();
-		descripcion.setBounds(90, 200, 400, 30);
+		descripcion.setBounds(90, 200, 350, 30);
 		descripcion.setName("campoDescripcion");
 		descripcion.setText("");
 		descripcion.setVisible(true);
@@ -213,6 +211,7 @@ public class ListaNodos extends JFrame implements ActionListener {
 		botonNuevo.setName("botonNuevo");
 		botonNuevo.setText("Nuevo");
 		botonNuevo.setVisible(true);
+		botonNuevo.addActionListener(this);
 		this.add(botonNuevo);
 		
 		botonRemover = new JButton();
@@ -220,6 +219,7 @@ public class ListaNodos extends JFrame implements ActionListener {
 		botonRemover.setName("botonRemover");
 		botonRemover.setText("Remover");
 		botonRemover.setVisible(true);
+		botonRemover.addActionListener(this);
 		this.add(botonRemover);
 		
 		botonSalvar = new JButton();
@@ -227,19 +227,119 @@ public class ListaNodos extends JFrame implements ActionListener {
 		botonSalvar.setName("botonSalvar");
 		botonSalvar.setText("Salvar");
 		botonSalvar.setVisible(true);
+		botonSalvar.addActionListener(this);
 		this.add(botonSalvar);
 		
-		this.setVisible(true);
+		botonEditar = new JButton();
+		botonEditar.setBounds(350, 400, 90, 30);
+		botonEditar.setName("botonEditar");
+		botonEditar.setText("Editar");
+		botonEditar.setVisible(true);
+		botonEditar.addActionListener(this);
+		this.add(botonEditar);
 		
+		botonEditar.setEnabled(false);
+		botonSalvar.setEnabled(false);
+		botonRemover.setEnabled(false);
+		
+		total.setEnabled(false);
+		codigo.setEnabled(false);
+		descripcion.setEnabled(false);
+		precio.setEnabled(false);
+		cantidad.setEnabled(false);
 	}
 	
     public static void main(String args[]) {
-        System.out.println("Miku daoyou");
 		ListaNodos objeto = new ListaNodos();
 		objeto.setVisible(true);
     }
 	@Override
-	public void actionPerformed(ActionEvent e){
-		//Eventos
-	}
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == botonBuscar) {
+            botonEditar.setEnabled(true);
+            botonSalvar.setEnabled(false);
+            botonRemover.setEnabled(true);
+            
+            codigo.setEnabled(false);
+            descripcion.setEnabled(false);
+            precio.setEnabled(false);
+            cantidad.setEnabled(false);
+            
+            Productos p = lista.Buscar(codigoBuscar.getText());
+            if (p != null) {
+                codigo.setText(p.getCodigo());
+                descripcion.setText(p.getDescripcion());
+                precio.setText(String.valueOf(p.getPrecio()));
+                cantidad.setText(String.valueOf(p.getCantidad()));
+                total.setText(String.valueOf(p.getCantidad() * p.getPrecio()));
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontró registro");
+            }
+        } else if (e.getSource() == botonNuevo) {
+            botonSalvar.setEnabled(true);
+            botonRemover.setEnabled(false);
+            botonEditar.setEnabled(false);
+            
+            codigo.setEnabled(true);
+            descripcion.setEnabled(true);
+            precio.setEnabled(true);
+            cantidad.setEnabled(true);
+            
+            codigo.setText("");
+            descripcion.setText("");
+            precio.setText("");
+            cantidad.setText("");
+            total.setText("");
+        } else if (e.getSource() == botonEditar) {
+            botonSalvar.setEnabled(true);
+            botonNuevo.setEnabled(false);
+            botonRemover.setEnabled(false);
+            
+            codigo.setEnabled(true);
+            descripcion.setEnabled(true);
+            precio.setEnabled(true);
+            cantidad.setEnabled(true);
+        } else if (e.getSource() == botonRemover) {
+            if (lista.Remover(codigoBuscar.getText()) == 0) {
+                JOptionPane.showMessageDialog(null, "Registro eliminado");
+                codigo.setText("");
+                descripcion.setText("");
+                precio.setText("");
+                cantidad.setText("");
+                total.setText("");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontró");
+            }
+        } else if (e.getSource() == botonSalvar) {
+            Productos p = lista.Buscar(codigo.getText());
+            if (p != null) { // Si el producto ya existe, estamos editando
+                p.setDescripcion(descripcion.getText());
+                p.setPrecio(Double.parseDouble(precio.getText()));
+                p.setCantidad(Double.parseDouble(cantidad.getText()));
+                total.setText(String.valueOf(p.getCantidad() * p.getPrecio()));
+                JOptionPane.showMessageDialog(this, "Producto actualizado");
+            } else { // Caso en que estamos creando un nuevo producto
+                Productos nuevoProducto = new Productos();
+                nuevoProducto.setCodigo(codigo.getText());
+                nuevoProducto.setDescripcion(descripcion.getText());
+                nuevoProducto.setPrecio(Double.parseDouble(precio.getText()));
+                nuevoProducto.setCantidad(Double.parseDouble(cantidad.getText()));
+                lista.Agregar(nuevoProducto);
+                JOptionPane.showMessageDialog(this, "Producto registrado");
+            }
+            
+            // Limpia los campos
+            codigo.setText("");
+            descripcion.setText("");
+            precio.setText("");
+            cantidad.setText("");
+            total.setText("");
+            
+            // Restablece los botones
+            botonNuevo.setEnabled(true);
+            botonEditar.setEnabled(false);
+            botonSalvar.setEnabled(false);
+            botonRemover.setEnabled(false);
+        }
+    }
 }
